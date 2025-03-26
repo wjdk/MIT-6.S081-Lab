2d77a61d8c9a0f2c1edd220c5b7948d2f7aa1915
root <root@Andy.>
2025-03-26 12:02:53 +0800

    Utli lab find done

---
+++ user/find.c
@@ -0,0 +1,84 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/fs.h"
+
+char*
+fmtname(char *path)
+{
+  static char buf[DIRSIZ+1];
+  char *p;
+
+  // Find first character after last slash.
+  for(p=path+strlen(path); p >= path && *p != '/'; p--)
+    ;
+  p++;
+
+  // Return blank-padded name.
+  if(strlen(p) >= DIRSIZ)
+    return p;
+  memmove(buf, p, strlen(p));
+  memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+  return buf;
+}
+
+void
+find(char *path,char *target_name)
+{
+  char buf[512], *p;
+  int fd;
+  struct dirent de;
+  struct stat st;
+  if((fd = open(path, 0)) < 0){
+    fprintf(2, "find: cannot open %s\n", path);
+    return;
+  }
+
+  if(fstat(fd, &st) < 0){
+    fprintf(2, "find: cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+  if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+    printf("find: path too long\n");
+    close(fd);
+    return ;
+  }
+  strcpy(buf, path);
+  p = buf+strlen(buf);
+  *p++ = '/';
+  while(read(fd, &de, sizeof(de)) == sizeof(de)){
+    if(de.inum == 0)
+      continue;
+    memmove(p, de.name, DIRSIZ);
+    p[DIRSIZ] = 0;
+    if(stat(buf, &st) < 0){
+      printf("find: cannot stat %s\n", buf);
+      continue;
+    }
+    // printf("test: %s\n",de.name);
+    // printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
+    switch(st.type){
+      case T_FILE:
+        if(strcmp(de.name,target_name)==0){
+          printf("%s\n",buf);
+        }
+        break;
+      case T_DIR:
+        if(strcmp(de.name,".")!=0 && strcmp(de.name,"..")!=0){
+          find(buf,target_name);
+        } 
+        break;
+    }
+  }
+  close(fd);
+}
+int main(int argc, char *argv[])
+{
+    if(argc<=2){
+        printf("too few arguments\n");
+        exit(1);
+    }
+    find(argv[1],argv[2]);
+    exit(0);
+}
